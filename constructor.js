455c4fbcaf1eb42ec7b2bd7f6e63a082d082295b
Tausi Ahmed <tausifahmed49@gmail.com>
2021-01-13 04:14:13 +0600

    constructor

---
+++ constructor.js
@@ -0,0 +1,8 @@
+function Book(title, author, year) {
+    this.title = title
+    this.author = author
+    this.year = year
+}
+
+const book1 = new Book('Book One', 'Tausif', '2021')
+console.log(book1)
